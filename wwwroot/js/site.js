8119feaace4b5cda0644b0a40d17b0e46ce16474
Ansar <ansarnisargill@gmail.com>
2019-10-09 12:43:36 +0500

    email sending updated

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -30,10 +30,20 @@ function ShowSecondModel(s) {
     $('#exampleModalLabel').empty();
     $('#exampleModalLabel').append(`When do you want to apply for ${s}. `);
     let Content = ``;
- 
-    Content = Content + `<button type="button" class="btn col-12 btn-outline-primary m-1">During One Week </button><button type="button" class="btn m-1 col-12 btn-outline-secondary">During 1 Month </button><button type="button" class="btn btn-outline-success m-1 col-12">General Inquiry </button>`;
+
+    Content = Content + `<button type="button" class="btn col-12 btn-outline-primary m-1 " onclick="sendEmail('${s}','One Week')">During One Week </button><button type="button" class="btn m-1 col-12 btn-outline-secondary" onclick="sendEmail('${s}','One Month')">During 1 Month </button><button type="button" class="btn btn-outline-success m-1 col-12" onclick="sendEmail('${s}','General')">General Inquiry </button>`;
     
     $('#modelBody').empty();
     $('#modelBody').append(Content);
     $('#exampleModal').modal();
+}
+
+function sendEmail(service, duration) {
+    let name = document.getElementById("Name").value;
+    let phone = document.getElementById("Phone").value;
+    let email = document.getElementById("Email").value;
+    let url = `https://voltaic-dance.glitch.me/send?service=${service}&duration=${duration}&email=${email}&name=${name}&phone=${phone}`;
+    console.log(url);
+    fetch(url).then(alert("email sent"));
+
 }
